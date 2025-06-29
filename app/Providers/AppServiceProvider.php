0591d3ebbb96a0e5c50515c19d59026efb046c1a
Travis CI <travis@travis-ci.org>
2025-06-30 01:30:11 +0700

    Final fix: Force HTTPS scheme for all generated URLs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if ($this->app->environment('production')) {
